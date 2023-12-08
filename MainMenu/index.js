14cb50b2380b8e0525da88bc6c6e128c348a21a9
Dreuzzz1 <147434513+Dreuzzz1@users.noreply.github.com>
2023-12-08 09:35:52 +0100

    Add files via upload

--- MainMenu/index.js
+++ MainMenu/index.js
@@ -5,8 +5,14 @@ function QuitGame() {
 function Credits() {
     const Credits = document.getElementById("Credits");
     
-    if (Credits.innerHTML == '')
+    if (Credits.innerHTML == '') {
         Credits.innerHTML = '<iframe class="Credit_Style" src="Credits/Credits.html" id="Credits" frameborder="0" width="500px" height="275px"></iframe>'
+        document.body.style.background = (0, 0, 0, 1);
+    }
     else
         Credits.innerHTML = '';
+}
+
+function StartGame() {
+    window.open("../Play/index.html")
 }
