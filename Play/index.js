47c935d9546dcb73d0b5a29adfaaa0ff8122c987
Dreuzzz1 <147434513+Dreuzzz1@users.noreply.github.com>
2023-12-12 23:27:19 +0100

    Topbar iframe

--- Play/index.js
+++ Play/index.js
@@ -9,8 +9,15 @@ function openCode() {
     //let newWin = window.open("../Code/index.html", "Close", "width=450,height=350");
     const popup = document.getElementById("popup");
     
+
+
     if (popup.innerHTML == '')
         popup.innerHTML = '<iframe class ="inline-code" src="../Code/index.html" id="story-readin" frameborder="0" width="450px" height="350px"></iframe>';
     else
         popup.innerHTML = '';
+
+    }
+
+function parentwindow() {
+    window.parent.document.getElementById('actions-buttons').click();
 }
