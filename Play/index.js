7f69b329ca6306f3ae7eef4836c3c392468b7955
nickel-dev <92646300+nickel-dev@users.noreply.github.com>
2023-12-13 11:16:20 +0100

    changes

--- Play/index.js
+++ Play/index.js
@@ -1,23 +1,28 @@
-function getIframeContent(frameId) {
-    const frameObj = document.getElementById(frameId);
-    const frameContent = frameObj.contentWindow.document.body.innerHTML;
-    alert("frame content : " + frameContent);
-    return frameContent;
-}
-
 function openCode() {
     //let newWin = window.open("../Code/index.html", "Close", "width=450,height=350");
     const popup = document.getElementById("popup");
     
-
-
     if (popup.innerHTML == '')
         popup.innerHTML = '<iframe class ="inline-code" src="../Code/index.html" id="story-readin" frameborder="0" width="450px" height="350px"></iframe>';
     else
         popup.innerHTML = '';
+}
 
+function gotoStory(index) {
+    const story = document.getElementById("story-readin");
+    story.setAttribute("src", "story/" + index + ".txt")
+    let name = "";
+
+    switch (index) {
+        case 0: name = "house-inside"; break;
+        case 1: name = "house-inside-code"; break;
+        case 2: name = "won-screen"; break;
+        case 3: name = "won-screen"; break;
+        case 4: name = "won-screen"; break;
+        case 5: name = "won-screen"; break;
+        case 6: name = "won-screen"; break;
+        default: name = "won-screen"; break;
     }
 
-function parentwindow() {
-    window.parent.document.getElementById('actions-buttons').click();
+    document.body.style.backgroundImage = "url('../img/" + name + ".png')";
 }
